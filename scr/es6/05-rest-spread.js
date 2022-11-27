4bf6299c3c0b4228690a3a2dafe8ad6bf7b1e6f0
Eduardo Díaz <eduaedodiaz@gmail.com>
2022-11-27 06:10:16 -0600

    Clase 05

---
+++ scr/es6/05-rest-spread.js
@@ -0,0 +1,10 @@
+//arrays destructuring
+let fruits = ["apple", "banana"];
+let [a, b] = fruits;
+console.log(a, fruits[1]);
+
+//object destructuring
+
+let user = { username: "Lalo", age: 30 };
+let { username, age } = user;
+console.log(username, user.age);
