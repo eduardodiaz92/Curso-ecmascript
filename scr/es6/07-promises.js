8fda0ba21e9697cc45627f8b1791e1a11cda4131
Eduardo Díaz <eduaedodiaz@gmail.com>
2022-11-27 09:20:32 -0600

    Clase 07

---
+++ scr/es6/07-promises.js
@@ -0,0 +1,13 @@
+const anotherFunction = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve("Hey!");
+    } else {
+      reject("whoops!");
+    }
+  });
+};
+
+anotherFunction()
+  .then((response) => console.log(response))
+  .catch((err) => console.log(err));
