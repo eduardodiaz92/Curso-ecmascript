6b85d26762d3f5c8d56a61dae484f6bb5213d9ea
Eduardo Díaz <eduaedodiaz@gmail.com>
2022-11-27 06:28:01 -0600

    spread operator

--- scr/es6/05-rest-spread.js
+++ scr/es6/05-rest-spread.js
@@ -8,3 +8,21 @@ console.log(a, fruits[1]);
 let user = { username: "Lalo", age: 30 };
 let { username, age } = user;
 console.log(username, user.age);
+
+//spread operator
+
+let person = { name: "Lalo", age: 30 };
+let country = "MX";
+
+let data = { id: 1, ...person, country };
+console.log(data);
+
+//rest
+
+function sum(num, ...values) {
+  console.log(values);
+  console.log(num + values[0]);
+  return num + values[0];
+}
+
+sum(1, 1, 2, 3);
