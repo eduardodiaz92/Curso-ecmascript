1feb8688cbff83b9bb877f8d092723ce8270b1fb
Eduardo Díaz <eduaedodiaz@gmail.com>
2022-11-28 04:52:59 -0600

    Clase 05

---
+++ scr/es8/05-async-function.js
@@ -0,0 +1,17 @@
+const fbAsync = () => {
+  return new Promise((resolve, reject) => {
+    true
+      ? setTimeout(() => resolve("AsynC!!", 2000))
+      : reject(new Error("Error!"));
+  });
+};
+
+const anortherFn = async () => {
+  const something = await fbAsync();
+  console.log(something);
+  console.log("hello");
+};
+
+console.log("Before");
+anortherFn();
+console.log("after");
