6f68bfeb9a650450f8700b6deb7a38ee9806ad78
Eduardo Díaz <eduaedodiaz@gmail.com>
2022-11-29 11:32:19 -0600

    Clase 02 y 03

---
+++ scr/es11/02-bigint.js
@@ -0,0 +1,5 @@
+const aBigNumber = 3426547563465345999999999999999999999999n;
+const aBigNumber2 = BigInt(34265475634653459999999999999999999999);
+
+console.log(aBigNumber);
+console.log(aBigNumber2);
